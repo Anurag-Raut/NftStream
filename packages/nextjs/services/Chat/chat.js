b855ad3553abbb486150111a0924115f87bf25aa
Anurag-Raut <rautab0@gmail.com>
2023-07-13 18:36:25 +0530

    chat-socket

--- packages/nextjs/services/Chat/chat.js
+++ packages/nextjs/services/Chat/chat.js
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 import io from 'socket.io-client'
-const socket=io('http://localhost:5000');
+const socket=io('https://streamvault.site:4999');
 
 
 const joinRoom= async ({id})=>{
