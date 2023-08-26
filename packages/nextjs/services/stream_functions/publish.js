7ab185e9289eac85ef670a02517b73c4ba882a28
Anurag-Raut <rautab0@gmail.com>
2023-08-26 18:19:58 +0530

    rtmp-hls

--- packages/nextjs/services/stream_functions/publish.js
+++ packages/nextjs/services/stream_functions/publish.js
@@ -173,7 +173,7 @@ const payload = {
   async function publishHelper(payload){
     console.log(payload,'payload')
     let url;
-   url='https://streamvault.site:3499/publish';
+   url='http://localhost:3500/publish';
     
       try {
           const response=await axios.post(url, payload)
@@ -195,7 +195,7 @@ const payload = {
 
   function startStreaming(stream,publishId){
    
-     socket=io.connect('https://streamvault.site:3499', { query: { id: publishId } });
+     socket=io.connect('http://localhost:3500', { query: { id: publishId } });
     // let combined = new MediaStream([...stream.getTracks()]);
 
  console.log(publishId,'publishId')
